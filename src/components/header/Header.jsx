8bd31b45bda0bb30c0161628799bb0043da68bec
fabiodrneles <fabiodrneles@gmail.com>
2024-01-02 19:13:23 -0300

    chore: correção do css global, havia um espaço entre o valor e a unidade de medida, ex: 1 rem; Corrigido.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from 'react';
+import "./header.css";
 
 const Header = () => {
     return (
-        <div>Header</div>
-    )
-}
+        <header className='header'>
+            <nav className="nav container">
+                <a href="index.html" className="nav__logo">Fabio</a>
 
-export default Header
+                <div className="nav__menu">
+                    <ul className="nav__list grid">
+                        <li className="nav__item">
+                            <a href="#home" className="nav__link">
+                                <i className="uil uil-estate nav__icon"></i> Home
+                            </a>
+                        </li>
+
+                        <li className="nav__item">
+                            <a href="About" className="nav__link">
+                                <i className="uil uil-user nav__icon"></i> About
+                            </a>
+                        </li>
+
+                        <li className="nav__item">
+                            <a href="#Skills" className="nav__link">
+                                <i className="uil uil-file-alt nav__icon"></i> Skills
+                            </a>
+                        </li>
+
+                        <li className="nav__item">
+                            <a href="#services" className="nav__link">
+                                <i className="uil uil-briefcase-alt nav__icon"></i> Services
+                            </a>
+                        </li>
+
+                        <li className="nav__item">
+                            <a href="#portfolio" className="nav__link">
+                                <i className="uil uil-scenery nav__icon"></i> Portfolio
+                            </a>
+                        </li>
+
+                        <li className="nav__item">
+                            <a href="#contact" className="nav__link">
+                                <i className="uil uil-message nav__icon"></i> Contact
+                            </a>
+                        </li>
+                    </ul>
+
+
+
+                    <i class="uil uil-times nav__close"></i>
+                </div>
+
+                <div className="nav__toggle"></div>
+            </nav>
+
+        </header>
+    );
+};
+
+export default Header;
