6f03f46655357148a79b18d91cccb8f801a605e7
fabiodrneles <fabiodrneles@gmail.com>
2024-01-21 15:22:15 -0300

    Feat: criando componente footer.

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">Fabio D'arcy</h1>
+
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="#portfolio" className="footer__link">
+              Projects
+            </a>
+          </li>
+
+          <li>
+            <a href="#testimonials" className="footer__link">
+              Testimonials
+            </a>
+          </li>
+        </ul>
+
+        <div className="footer__social">
+          <a
+            href="https://instagram.com"
+            className="footer__social-link"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <i class="bx bxl-instagram"></i>
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/fabiodrneles/"
+            className="footer__social-link"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <i class="bx bxl-linkedin"></i>
+          </a>
+
+          <a
+            href="https://github.com"
+            className="footer__social-link"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <i class="bx bxl-github"></i>
+          </a>
+        </div>
+
+        <span className="footer__copy">
+          &#169; Fabio D'arcy. All rigths reserved
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
