c66de9ab90887ad6d163b0679727d8338555fab4
fabiodrneles <fabiodrneles@gmail.com>
2024-01-21 16:28:15 -0300

    Feat: criando componente scrollup.

---
+++ src/components/scrollup/ScrollUp.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./scrollup.css";
+
+const ScrollUp = () => {
+  window.addEventListener("scroll", function () {
+    const ScrollUp = document.querySelector(".scrollup");
+    // when the scroll is higher than 560 viewport height, add the show-scroll class to a tag with the scroll-top class
+    if (this.scrollY >= 560) ScrollUp.classList.add("show-scroll");
+    else ScrollUp.classList.remove("show-scroll");
+  });
+  return (
+    <a href="#home" className="scrollup">
+      <i className="uil uil-arrow-up scrollup_icon"></i>
+    </a>
+  );
+};
+
+export default ScrollUp;
