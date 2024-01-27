30a00d91a3382e29f6bd753d266eb5c776837e75
fabiodrneles <fabiodrneles@gmail.com>
2024-01-27 00:26:18 -0300

    chore: add template in data.jsx

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -36,4 +36,103 @@ export const Data = [
       </a>
     ),
   },
+  {
+    id: 4,
+    image: Image3,
+    title: "Aplicação Next.js / React.js",
+    description: "Aplicação frontend construída com Next.js",
+    link: (
+      <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
+        ver mais
+      </a>
+    ),
+  },
+  {
+    id: 5,
+    image: Image2,
+    title: "Aplicação Angular",
+    description: "Aplicação frontend construída com Angular 2+",
+    link: (
+      <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
+        ver mais
+      </a>
+    ),
+  },
+  {
+    id: 6,
+    image: Image1,
+    title: "Aplicação Java",
+    description: "Aplicação backend construída com java, spring framework.",
+    link: (
+      <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
+        ver mais
+      </a>
+    ),
+  },
+  {
+    id: 7,
+    image: Image1,
+    title: "Aplicação Java",
+    description: "Aplicação backend construída com java, spring framework.",
+    link: (
+      <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
+        ver mais
+      </a>
+    ),
+  },
+  {
+    id: 8,
+    image: Image2,
+    title: "Aplicação Angular",
+    description: "Aplicação frontend construída com Angular 2+",
+    link: (
+      <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
+        ver mais
+      </a>
+    ),
+  },
+  {
+    id: 9,
+    image: Image3,
+    title: "Aplicação Next.js / React.js",
+    description: "Aplicação frontend construída com Next.js",
+    link: (
+      <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
+        ver mais
+      </a>
+    ),
+  },
+  {
+    id: 10,
+    image: Image3,
+    title: "Aplicação Next.js / React.js",
+    description: "Aplicação frontend construída com Next.js",
+    link: (
+      <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
+        ver mais
+      </a>
+    ),
+  },
+  {
+    id: 11,
+    image: Image2,
+    title: "Aplicação Angular",
+    description: "Aplicação frontend construída com Angular 2+",
+    link: (
+      <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
+        ver mais
+      </a>
+    ),
+  },
+  {
+    id: 12,
+    image: Image1,
+    title: "Aplicação Java",
+    description: "Aplicação backend construída com java, spring framework.",
+    link: (
+      <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
+        ver mais
+      </a>
+    ),
+  },
 ];
