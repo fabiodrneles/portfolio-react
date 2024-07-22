812bfd5f851cbd7ea746d866045c5cb1fc28ae5f
Fabio Dorneles <42509240+fabiodrneles@users.noreply.github.com>
2024-07-22 15:16:59 -0300

    atualizando componente backend para mobile

---
+++ src/components/skills/Mobile.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+
+const Backend = () => {
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title">Mobile Developer</h3>
+
+      <div className="skills__box">
+            <div className="skills__group">
+                <div className="skills__data">
+                <i class='bx bx-badge-check'></i>
+
+                <div>
+                    <h3 className="skills__name">Java</h3>
+                    <span className="skills__level">Intermediate</span>
+                </div>
+                </div>
+
+                <div>
+                    <h3 className="skills__name">Kotlin</h3>
+                    <span className="skills__level">Intermediate</span>
+                </div>
+                </div>
+
+                <div className="skills__data">
+                <i class='bx bx-badge-check'></i>
+
+                <div>
+                    <h3 className="skills__name">FlutterFlow</h3>
+                    <span className="skills__level">Intermediate</span>
+                </div>
+                </div>
+
+                <div className="skills__data">
+                <i class='bx bx-badge-check'></i>
+
+                <div>
+                    <h3 className="skills__name">JavaScript / TypeScript</h3>
+                    <span className="skills__level">Intermediate</span>
+                </div>
+                </div>
+            </div>
+
+            <div className="skills__group">
+                <div className="skills__data">
+                <i class='bx bx-badge-check'></i>
+
+                <div>
+                    <h3 className="skills__name">React Native</h3>
+                    <span className="skills__level">Intermediate</span>
+                </div>
+                </div>
+
+                <div className="skills__data">
+                <i class='bx bx-badge-check'></i>
+
+                <div>
+                    <h3 className="skills__name">Firebase</h3>
+                    <span className="skills__level">Advanced</span>
+                </div>
+                </div>
+
+                <div className="skills__data">
+                <i class='bx bx-badge-check'></i>
+
+                <div>
+                    <h3 className="skills__name">Android</h3>
+                    <span className="skills__level">Intermediate</span>
+                </div>
+                </div>
+
+                <div className="skills__data">
+                <i class='bx bx-badge-check'></i>
+
+                <div>
+                    <h3 className="skills__name">JetPack Compose</h3>
+                    <span className="skills__level">Intermediate</span>
+                </div>
+                </div>
+            </div>
+      </div>
+    </div>
+  )
+}
+
+export default Backend
