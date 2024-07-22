86104fd4bb57a180c8bd05718c967173cae8568b
Fabio Dorneles <42509240+fabiodrneles@users.noreply.github.com>
2024-07-22 14:52:54 -0300

    trocando termo PHP para fFlutterFlow

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -20,7 +20,7 @@ const Backend = () => {
                 <i class='bx bx-badge-check'></i>
 
                 <div>
-                    <h3 className="skills__name">PHP</h3>
+                    <h3 className="skills__name">FlutterFlow</h3>
                     <span className="skills__level">Intermediate</span>
                 </div>
                 </div>
