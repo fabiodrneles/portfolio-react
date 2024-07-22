d30cc6bf65db13de296e475d92569fa8575280ac
Fabio Dorneles <42509240+fabiodrneles@users.noreply.github.com>
2024-07-22 15:44:36 -0300

    update component mobile

--- src/components/skills/Mobile.jsx
+++ src/components/skills/Mobile.jsx
@@ -54,6 +54,15 @@ const Mobile = () => {
                     <span className="skills__level">Intermediate</span>
                 </div>
                 </div>
+
+                <div className="skills__data">
+                <i class='bx bx-badge-check'></i>
+
+                <div>
+                    <h3 className="skills__name">FlutterFlow</h3>
+                    <span className="skills__level">Intermediate</span>
+                </div>
+                </div>
             </div>
       </div>
   )
