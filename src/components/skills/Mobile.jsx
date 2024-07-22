a29dfcb74a0a605a2635338ab6f1597f7685a91c
Fabio Dorneles <42509240+fabiodrneles@users.noreply.github.com>
2024-07-22 15:42:38 -0300

    update, component item

--- src/components/skills/Mobile.jsx
+++ src/components/skills/Mobile.jsx
@@ -29,7 +29,7 @@ const Mobile = () => {
                 <i class='bx bx-badge-check'></i>
 
                 <div>
-                    <h3 className="skills__name">FlutterFlow</h3>
+                    <h3 className="skills__name">JetPack Compose</h3>
                     <span className="skills__level">Intermediate</span>
                 </div>
                 </div>
@@ -52,24 +52,6 @@ const Mobile = () => {
                 <div>
                     <h3 className="skills__name">Firebase</h3>
                     <span className="skills__level">Advanced</span>
-                </div>
-                </div>
-
-                <div className="skills__data">
-                <i class='bx bx-badge-check'></i>
-
-                <div>
-                    <h3 className="skills__name">Android</h3>
-                    <span className="skills__level">Intermediate</span>
-                </div>
-                </div>
-
-                <div className="skills__data">
-                <i class='bx bx-badge-check'></i>
-
-                <div>
-                    <h3 className="skills__name">JetPack Compose</h3>
-                    <span className="skills__level">Intermediate</span>
                 </div>
                 </div>
             </div>
