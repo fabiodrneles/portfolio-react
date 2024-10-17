a4a24aba300112bf2d13a2519bfd1939b1792c32
fabiodrneles <fabiodrneles@gmail.com>
2024-10-16 21:22:35 -0300

    refactor in projects blocks

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -1,147 +1,48 @@
 import Image1 from "../../assets/java.png";
 import Image2 from "../../assets/angular.png";
-import Image3 from "../../assets/nextjs.png";
 import Image4 from "../../assets/react-native-logo.png";
 import Image5 from "../../assets/kotlin.png";
+import Image6 from "../../assets/reactjs2.png";
 
 export const Data = [
   {
     id: 1,
     image: Image1,
-    title: "Aplicação Java",
-    description: "Aplicação backend construída com java, spring framework.",
+    title: "API REST with Spring Boot",
+    description: "Basic REST API built with Spring Boot to enhance my API development skills.",
     link: (
-      <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/fabiodrneles/api-java-spring-boot" target="_blank" rel="noopener noreferrer">
         ver mais
       </a>
     ),
   },
   {
     id: 2,
-    image: Image2,
-    title: "Aplicação Angular",
-    description: "Aplicação frontend construída com Angular 2+",
+    image: Image6,
+    title: "Portfolio made in React",
+    description: "A React.JS portfolio showcasing my expertise in building React applications.",
     link: (
-      <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/fabiodrneles/portfolio-react" target="_blank" rel="noopener noreferrer">
         ver mais
       </a>
     ),
   },
   {
     id: 3,
-    image: Image3,
-    title: "Aplicação Next.js / React.js",
-    description: "Aplicação frontend construída com Next.js",
+    image: Image2,
+    title: "Aplicação Angular",
+    description: "Aplicação frontend construída com Angular 2+",
     link: (
-      <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/fabiodrneles" target="_blank" rel="noopener noreferrer">
         ver mais
       </a>
     ),
   },
   {
     id: 4,
-    image: Image3,
-    title: "Aplicação Next.js / React.js",
-    description: "Aplicação frontend construída com Next.js",
-    link: (
-      <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
-        ver mais
-      </a>
-    ),
-  },
-  {
-    id: 5,
-    image: Image2,
-    title: "Aplicação Angular",
-    description: "Aplicação frontend construída com Angular 2+",
-    link: (
-      <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
-        ver mais
-      </a>
-    ),
-  },
-  {
-    id: 6,
-    image: Image1,
-    title: "Aplicação Java",
-    description: "Aplicação backend construída com java, spring framework.",
-    link: (
-      <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
-        ver mais
-      </a>
-    ),
-  },
-  {
-    id: 7,
-    image: Image1,
-    title: "Aplicação Java",
-    description: "Aplicação backend construída com java, spring framework.",
-    link: (
-      <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
-        ver mais
-      </a>
-    ),
-  },
-  {
-    id: 8,
-    image: Image2,
-    title: "Aplicação Angular",
-    description: "Aplicação frontend construída com Angular 2+",
-    link: (
-      <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
-        ver mais
-      </a>
-    ),
-  },
-  {
-    id: 9,
-    image: Image3,
-    title: "Aplicação Next.js / React.js",
-    description: "Aplicação frontend construída com Next.js",
-    link: (
-      <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
-        ver mais
-      </a>
-    ),
-  },
-  {
-    id: 10,
-    image: Image3,
-    title: "Aplicação Next.js / React.js",
-    description: "Aplicação frontend construída com Next.js",
-    link: (
-      <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
-        ver mais
-      </a>
-    ),
-  },
-  {
-    id: 11,
-    image: Image2,
-    title: "Aplicação Angular",
-    description: "Aplicação frontend construída com Angular 2+",
-    link: (
-      <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
-        ver mais
-      </a>
-    ),
-  },
-  {
-    id: 12,
-    image: Image1,
-    title: "Aplicação Java",
-    description: "Aplicação backend construída com java, spring framework.",
-    link: (
-      <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
-        ver mais
-      </a>
-    ),
-  },
-  {
-    id: 13,
     image: Image4,
     title: "Aplicação React Native",
-    description: "Este projeto apresenta uma interface para um aplicativo de delivery. O objetivo deste repositório não é fornecer uma aplicação completa, mas sim ser usado como peça de portfólio.",
+    description: "This React Native project provides a delivery app templateThis React Native project provides a delivery app template.",
     link: (  
       <a href="https://github.com/fabiodrneles/food" target="_blank" rel="noopener noreferrer">
         ver mais
@@ -150,12 +51,12 @@ export const Data = [
   },
 
   {
-    id: 14,
+    id: 5,
     image: Image5,
     title: "Aplicação Kotlin",
     description: "Aplicação Mobile Android com Kotlin. Confira os detalhes do projeto no repositório",
     link: (
-      <a href="http://www.google.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/fabiodrneles" target="_blank" rel="noopener noreferrer">
         ver mais
       </a>
     ),
